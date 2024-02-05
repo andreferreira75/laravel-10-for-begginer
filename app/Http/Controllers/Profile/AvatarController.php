9af360fdd04efe14c24d8ee5cb7c06af838f6aec
André Ferreira <andre.ferreira@codeware.pt>
2024-02-05 08:36:14 +0000

    step 19 completed

---
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Profile;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateAvatarRequest;
+use Illuminate\Http\Request;
+
+class AvatarController extends Controller
+{
+    public function update(UpdateAvatarRequest $request){
+
+        $path = $request->file('avatar') ->store('avatars');
+        auth()->user()->update(['avatar' => storage_path('app')."/$path"]);
+        $request->file('avatar')->store('avatars');
+
+        return redirect(route('profile.edit'))->with('message', 'Avatar is updated');
+    }
+}
